ea36918a6ce1a4dac33a68609b98e92f3154b462
Joonas Pessi <joonas.pessi@futurice.com>
2014-09-14 22:36:27 +0300

    Improved responsive styles for workouts

--- public/src/components/workouts/Workouts.jsx
+++ public/src/components/workouts/Workouts.jsx
@@ -8,7 +8,12 @@ var Workout = React.createClass({
     render: function render() {
         return (
            <li className="workout">
-           <h1>{this.props.name} </h1>
+               <h1>{this.props.name}</h1>
+               <ul className="summary">
+                <li>{this.props.day}</li> 
+                <li>{this.props.distance} km </li>   
+                <li>{this.props.avgHr} bpm </li>   
+               </ul>
            </li>
         );
     }
@@ -25,11 +30,13 @@ var Navbar = React.createClass({
         
         return (
             <ul className="workouts"> 
-                <Workout name="Nokia" />
-                <Workout name="Nokia" />
-                <Workout name="Nokia" />
-                <Workout name="Nokia" />
-                <Workout name="Nokia" />
+                <Workout name="Takakalliot" day="14.9.2014" distance="40" avgHr="155" /> 
+                <Workout name="Nokia" day="15.9.2014" distance="40" avgHr="141"/> 
+                <Workout name="Nokia" day="15.9.2014" distance="40" avgHr="142"/> 
+                <Workout name="Nokia" day="15.9.2014" distance="40" avgHr="143"/> 
+                <Workout name="Nokia" day="15.9.2014" distance="40" avgHr="144"/> 
+                <Workout name="Nokia" day="15.9.2014" distance="40" avgHr="145"/> 
+                <Workout name="Nokia" day="15.9.2014" distance="40" avgHr="146"/> 
             </ul>
         );
     }
