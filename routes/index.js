cdd3fcc46e918ca616cd34ae6bc20aa4fb58ec21
Joonas Pessi <joonas.pessi@futurice.com>
2014-10-02 21:00:34 +0300

    Removed console logs

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,6 @@ function getDaysInMonth(month, year, day) {
          // Since no month has fewer than 28 days
          var date = new Date(year, month, day);
          var days = [];
-         console.log('month', month, 'date.getMonth()', date.getMonth())
          while (date.getMonth() === month) {
             days.push(moment(new Date(date)));
             date.setDate(date.getDate() + 1);
@@ -20,7 +19,6 @@ var days = getDaysInMonth(moment().month(), moment().year(), moment().day());
 
 for(i = 0; i < days.length; ++i) {
 	var workout = {id: i, name: "Test data " + i, day: days[i].format('DD-MM-YYYY'), distance: i, avgHr: i*3 };
-	console.log(workout);
 	workoutDummyList.push(workout);
 }
 
